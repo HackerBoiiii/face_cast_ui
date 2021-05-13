a32de016bf0800a3a8f91749428e1b03788d04ad
rakeshrawat <rakeshrawat456@gmail.com>
2021-05-13 13:20:53 +0530

    final

--- lib/friendsfollowingscreens/friendfollowingscreens.dart
+++ lib/friendsfollowingscreens/friendfollowingscreens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class FriendScreen extends StatefulWidget {
   @override
   _FriendScreenState createState() => _FriendScreenState();
@@ -7,136 +8,144 @@ class FriendScreen extends StatefulWidget {
 class _FriendScreenState extends State<FriendScreen> {
   @override
   Widget build(BuildContext context) {
-    Size size=MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return DefaultTabController(
       initialIndex: 0,
       length: 2,
       child: Scaffold(
         backgroundColor: Colors.white,
-appBar: AppBar(
-  backgroundColor: Colors.white,
-  elevation: 0,
-  actions: [
-
-      Icon(Icons.search,color: Colors.grey,size: size.height*0.05,),
-      SizedBox(
-        width: size.width*0.07  ,
-      ),
-      Icon(Icons.mail_outline_sharp,color: Colors.grey,size: size.height*0.05,)
-  ],
-  bottom: PreferredSize(
-      child: Container(
-        width: size.width/1.2,
-        height: 35,
-        child: TabBar(
-          indicator: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            gradient: LinearGradient(
-              colors: [
-                Colors.redAccent,Colors.orange,
-              ]
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                  size: size.height * 0.05,
+                ),
+                SizedBox(
+                  width: size.width * 0.07,
+                ),
+                Icon(
+                  Icons.mail_outline_sharp,
+                  color: Colors.grey,
+                  size: size.height * 0.05,
+                )
+              ],
             )
-          ),
-          tabs: [
-            Container(
-              child: Text('Following',style: TextStyle(
-                color: Colors.black
-              ),),
+
+          ],
+          bottom: PreferredSize(
+            child: Container(
+              color: Colors.white,
+              width: size.width / 1.2,
+              height: 30,
+              child: TabBar(labelColor: Colors.white,
+
+                indicatorColor: Colors.white,
+                unselectedLabelColor: Colors.white,
+indicatorWeight: 10,
+                indicator: BoxDecoration(
+                    color: Colors.grey),
+                tabs: [
+                  Container(
+                    child: Text(
+                      'Following',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Friends',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  )
+                ],
+              ),
             ),
-            Container(
-              child: Text('Friends',style: TextStyle(
-                color: Colors.black
-              ),),
-            )
-
-          ],
+            preferredSize: Size(
+              size.width,
+              20.0,
+            ),
+          ),
         ),
-      ),
-      preferredSize: Size(size.width, 20.0,),),
-      ),
         body: TabBarView(
-          children: [
-            Following(),
-            Friends()
-          ],
+          children: [Following(), Friends()],
         ),
-
       ),
     );
   }
 
-  Widget Following(){
-    Size size=MediaQuery.of(context).size;
+  Widget Following() {
+    Size size = MediaQuery.of(context).size;
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image: NetworkImage('https://googleflutter.com/sample_image.jpg'),
-                  fit: BoxFit.fill
-              ),
-            ),
+            width: size.width/2,
+            height: size.height/3,
+           child: Image.network('https://1.bp.blogspot.com/-XsL8zuZYOUQ/YJv600Zm8vI/AAAAAAAADI4/FtraLYbVknodouKk7YmVk-u6oa0KoNW9QCLcBGAsYHQ/s685/Untitled-1.png',fit: BoxFit.fill,),
           ),
-SizedBox(
-  height: 50,
-),
-Text('Moment no friends on LIVE'),
           SizedBox(
-            height: 15,
+            height: size.height/18,
+          ),
+          Text('Moment no friends on LIVE',style: TextStyle(color: Colors.grey)),
+          SizedBox(
+            height: size.height/18,
           ),
           ClipRRect(
             borderRadius: BorderRadius.circular(15),
             child: Container(
-              height: size.height/16,
+              height: size.height / 16,
               color: Colors.deepPurpleAccent,
-              child: Center(child: Text('Find some on Live',style: TextStyle(
-                color: Colors.white
-              ),)),
-              width: size.width/1.3,
-
+              child: Center(
+                  child: Text(
+                'Find some on LIVE',
+                style: TextStyle(fontWeight: FontWeight.w400,
+                    color: Colors.white),
+              )),
+              width: size.width / 1.3,
             ),
           )
         ],
       ),
     );
   }
-  Widget Friends(){
-    Size size=MediaQuery.of(context).size;
+
+  Widget Friends() {
+    Size size = MediaQuery.of(context).size;
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image: NetworkImage('https://googleflutter.com/sample_image.jpg'),
-                  fit: BoxFit.fill
-              ),
-            ),
+            width: size.width/2,
+            height: size.height/3,
+            child: Image.network('https://1.bp.blogspot.com/-XsL8zuZYOUQ/YJv600Zm8vI/AAAAAAAADI4/FtraLYbVknodouKk7YmVk-u6oa0KoNW9QCLcBGAsYHQ/s685/Untitled-1.png',fit: BoxFit.fill,),
           ),
           SizedBox(
-            height: 50,
+            height: size.height/18,
           ),
-          Text('Moment no friends on LIVE'),
+          Text('Moment no friends on LIVE',style: TextStyle(color: Colors.grey)),
           SizedBox(
-            height: 15,
+            height: size.height/18,
           ),
           ClipRRect(
             borderRadius: BorderRadius.circular(15),
             child: Container(
-              height: size.height/13,
+              height: size.height / 16,
               color: Colors.deepPurpleAccent,
-              child: Center(child: Text('Find some on Live')),
-              width: size.width/1.3,
-
+              child: Center(
+                  child: Text(
+                    'Find some on LIVE',
+                    style: TextStyle(fontWeight: FontWeight.w400,
+                        color: Colors.white),
+                  )),
+              width: size.width / 1.3,
             ),
           )
         ],
